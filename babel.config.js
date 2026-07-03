c5fc53db5e8c011dbc2db4cb031dfbb468d52906
Andrew Tran <andrewtrann233@gmail.com>
2026-07-03 03:53:41 -0700

    build(configuration): configure @babel/preset-env

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                "useBuiltIns": "entry",
+                "corejs": "3.22"
+            }
+        ],
+        [
+            "@babel/preset-react",
+            {
+                "runtime": "automatic",
+            },
+        ],
+    ],
+}
